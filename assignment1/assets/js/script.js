b6d14e433c6d4e440c67eb904b7046ef8861de6b
sifaZhang <sifazhang.nz@gmail.com>
2025-08-11 21:15:41 +1200

    scene1

--- assignment1/assets/js/script.js
+++ assignment1/assets/js/script.js
@@ -1,3 +1,11 @@
 window.addEventListener("DOMContentLoaded", function() {
     // perform on-load actions here
 });
+
+const scene1 = document.getElementById('adScene1');
+const scene2 = document.getElementById('adScene2');
+const bannerScene1Text3 = document.getElementById('bannerScene1Text3');
+bannerScene1Text3.addEventListener('animationend', () => {
+    scene1.classList.add('hidden');
+    scene2.classList.remove('hidden');
+});
